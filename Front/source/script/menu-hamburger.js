f4b27641f468ea0f7bc0d7cb22c668233281d4bd
lrazafin <razafindravohitrat@proximity.bbdo.fr>
2017-06-07 17:15:30 +0400

    html folder for back

--- Front/source/script/menu-hamburger.js
+++
@@ -1,8 +0,0 @@
-//menu off
-$(".btn-menu-cancel").on('click', function() {
-  TweenLite.to(".menu-hamburger", 0.5, {left:-1500});
-});
-//menu on
-$(".btn-menu").on('click', function() {
-  TweenLite.to(".menu-hamburger", 0.3, {left:0});
-});
